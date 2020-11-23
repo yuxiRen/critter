eea5c86328ff36e63687c19a15f20212cbf8450e
yuxiRen <rita.ren.yuxi@gmail.com>
2020-11-22 23:32:29 -0800

    Create Entities that represent the storage needs of the application

---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,61 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Nationalized
+    private String name;
+    private String phoneNumber;
+    private String notes;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
+    private List<Pet> pets;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Employee.java
@@ -0,0 +1,52 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Nationalized
+    private String name;
+    @ElementCollection
+    private Set<EmployeeSkill> skills;
+    @ElementCollection
+    private Set<DayOfWeek> daysAvailable;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
