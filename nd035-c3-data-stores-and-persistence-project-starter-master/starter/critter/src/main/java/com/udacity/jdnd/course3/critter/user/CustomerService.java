5e500db76ab824c80fa4442f2661e4209926377e
yuxiRen <rita.ren.yuxi@gmail.com>
2020-11-23 23:02:49 -0800

    CustomerService

---
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -0,0 +1,20 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
+    }
+    public List<Customer> findAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
