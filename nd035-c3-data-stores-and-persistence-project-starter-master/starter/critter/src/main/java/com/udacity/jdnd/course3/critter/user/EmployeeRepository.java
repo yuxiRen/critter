b548182a66653a8ddc83649596d03542b96997cc
yuxiRen <rita.ren.yuxi@gmail.com>
2020-11-24 23:48:14 -0800

    update EmployeeRepository

--- nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRepository.java
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 @Repository
 @Transactional
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    public List<Employee> getEmployeesByDaysAvailable(DayOfWeek day);
 }
--- nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeService.java
+++ nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -15,9 +18,11 @@ public class EmployeeService {
     public Employee save(Employee employee) {
         return employeeRepository.save(employee);
     }
+
     public Employee findEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
     }
+
     public boolean setDaysAvailable(Set<DayOfWeek> availableDays, Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).orElse(null);
         if (employee == null) {
@@ -26,5 +31,15 @@ public class EmployeeService {
         employee.setDaysAvailable(availableDays);
         employeeRepository.save(employee);
         return true;
+    }
+    public List<Employee> findEmployeesAvailable(LocalDate date, Set<EmployeeSkill> employeeSkills) {
+        List<Employee> employees = employeeRepository.getEmployeesByDaysAvailable(date.getDayOfWeek());
+        List<Employee> foundEmployees = new ArrayList<>();
+        for (Employee employee : employees){
+            if(employee.getSkills().containsAll(employeeSkills)){
+                foundEmployees.add(employee);
+            }
+        }
+        return foundEmployees;
     }
 }
